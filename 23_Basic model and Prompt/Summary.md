75a2ed728f62cfdfab49fbca3952e8b3b3d200bf
Redolthfi <redolthfi@gmail.com>
2023-10-17 14:16:05 +0700

    Submit BasicModel&Prompt

---
+++ 23_Basic model and Prompt/Summary.md
@@ -0,0 +1,24 @@
+#BASIC MODEL & Prompt
+
+Basic Model OpenAI:
+
+1. Model Teks: Basic Model OpenAI adalah model kecerdasan buatan yang digunakan khusus untuk memproses teks.
+
+2. Kemampuan Pemrosesan Teks: Model ini dapat digunakan untuk berbagai tugas pemrosesan teks seperti generasi teks, penerjemahan, pemahaman bahasa alami, dan sebagainya.
+
+3. API: Basic Model OpenAI dapat diakses melalui API, yang memungkinkan pengembang untuk berinteraksi dengan model ini dalam aplikasi mereka.
+
+4. Bahasa yang Didukung: Model ini mendukung beberapa bahasa dan dapat digunakan dalam berbagai konteks yang melibatkan teks.
+
+Prompt Engineer:
+
+1. Teknik Optimasi Teks: Prompt Engineer adalah teknik yang digunakan untuk mengoptimalkan hasil output dari model kecerdasan buatan dengan memberikan prompt atau instruksi yang sesuai.
+
+2. Penggunaan pada Model AI: Teknik ini umumnya digunakan untuk model AI, seperti model OpenAI, untuk mengarahkan model dalam menghasilkan respons yang sesuai dengan instruksi yang diberikan.
+
+3. Customization: Prompt Engineer memungkinkan pengguna untuk menyesuaikan instruksi atau prompt sesuai dengan kebutuhan tugas tertentu.
+
+4. Peningkatan Kualitas Output: Dengan penggunaan yang tepat, Prompt Engineer dapat meningkatkan kualitas hasil yang dihasilkan oleh model AI, sehingga respons yang dihasilkan menjadi lebih sesuai dengan harapan pengguna.
+
+5. Penting dalam Aplikasi Teks: Teknik ini sering digunakan dalam berbagai aplikasi yang melibatkan pemrosesan teks, seperti chatbot, penerjemahan otomatis, dan tugas sejenisnya.
+
---
+++ 23_Basic model and Prompt/my-react-app/src/Pages/OpenAi.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import OpenAI from "openai";
+import AllInput from "../Component/InputanLabel";
+
+const FormAI = () => {
+  const openai = new OpenAI({
+    apiKey: "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",
+    dangerouslyAllowBrowser: true,
+  });
+
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const AIClick = async () => {
+    setLoading(true);
+    try {
+      const resp = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: prompt,
+        max_tokens: 250,
+      });
+      setResult(resp.choices[0].text);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="border border-black p-6 w-full flex flex-col space-y-4">
+        <AllInput
+          name="text_ai"
+          label="What is your question?"
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button
+          className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-700"
+          onClick={AIClick}
+        >
+          Submit
+        </button>
+        <p>{loading ? "AI is Loading...." : ""}</p>
+        {result && (
+          <div className="border border-black p-4">
+            <p className="text-xl font-semibold">Result:</p>
+            <p className="mt-2">{result}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FormAI;
